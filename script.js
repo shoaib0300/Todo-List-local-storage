2cfa2e7766dad5bc064ce31e83a612650c5819fb
Shoaib <shoaibsarwar187@gmail.com>
2023-11-18 22:54:49 +0100

    Complete Project with Delete and Animations

--- script.js
+++ script.js
@@ -2,19 +2,76 @@ const ul = document.querySelector('ul');
 const input = document.getElementById('item');
 
 let itemsArray = localStorage.getItem('items') ?
-JSON.parse(localStorage.getItem('items')) : [];
+  JSON.parse(localStorage.getItem('items')) : [];
 
-itemsArray.forEach(addTask);
+function addTask(task) {
+  const li = document.createElement('li');
+  li.classList.add('animated-element');
 
-function addTask(text){
-    const li = document.createElement('li');
-    li.textContent = text;
-    ul.appendChild(li);
+
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.id = 'checkboxID';
+  checkbox.checked = task.completed;
+
+  checkbox.addEventListener('change', function() {
+    task.completed = this.checked;
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+  });
+
+  const span = document.createElement('span');
+  span.textContent = task.text;
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'DELETE';
+  deleteButton.onclick = function() {
+    deleteTask(task);
+  };
+
+  li.appendChild(checkbox);
+  li.appendChild(span);
+  li.appendChild(deleteButton);
+  ul.appendChild(li);
 }
 
-function add(){
-    itemsArray.push(input.value);
-    localStorage.setItem('item', JSON.stringify('itemsArray'));
-    addTask(input.value);
-    input.value = ''
+function deleteTask(task) {
+  const index = itemsArray.indexOf(task);
+
+  if (index !== -1) {
+    itemsArray.splice(index, 1);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+    renderTasks();
+  }
 }
+
+function renderTasks() {
+  ul.innerHTML = '';
+  itemsArray.forEach(addTask);
+}
+
+function add() {
+  if (input.value !== '') {
+    const task = {
+      text: input.value,
+      completed: false
+    };
+    itemsArray.push(task);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+    addTask(task);
+    input.value = '';
+  }
+}
+
+function del() {
+  localStorage.removeItem('items');
+  ul.innerHTML = '';
+  itemsArray = [];
+}
+
+input.addEventListener("keydown", function(event) {
+  if (event.key == "Enter") {
+    add();
+  }
+});
+
+renderTasks();
